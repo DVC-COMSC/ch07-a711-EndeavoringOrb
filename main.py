1083196fced9892980dc61a70e652449f7a159e7
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-25 11:17:49 -0700

    2nd commit

--- main.py
+++ main.py
@@ -11,7 +11,21 @@ labels = ['Math', 'English', 'Physics', 'Computer']
 names = ['Bill', 'Mary']
 
 # ******************************
-# Make your code
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - 3*width/2, Math, width, label='Math')
+rects2 = ax.bar(x - width/2, English, width, label='English')
+rects3 = ax.bar(x + width/2, Physics, width, label='Physics')
+rects4 = ax.bar(x + 3*width/2, Computer, width, label='Computer')
+
+ax.set_ylabel('Scores')
+ax.set_title('Subject Scores')
+ax.set_xticks(x, names)
+ax.legend()
+
+ax.bar_label(rects1, padding=3)
+ax.bar_label(rects2, padding=3)
+ax.bar_label(rects3, padding=3)
+ax.bar_label(rects4, padding=3)
 # ******************************
 
 
